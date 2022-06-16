5ef89a57fd09e6ea42b7b1755410348c0b5e6743
enochrx <drreayy@gmail.com>
2022-06-16 02:06:03 +0100

    dice rolled

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
-//Selecting element by ID
+//Selecting element by Classes & IDs
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
